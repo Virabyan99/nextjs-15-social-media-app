f08368854f4d4b8289117d9e0a810d4faffac229
Andranik Virabyan <gmparstone99@gmail.com>
2024-10-11 10:17:21 -0800

    menu bar and post section added

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import PostEditor from "@/components/posts/editor/PostEditor";
 
 export default function Home() {
   return (
-    <main>Front page</main>
+    <main className="h-[200vh] w-full bg-red-50">
+      <div className="w-full">
+        <PostEditor/>
+      </div>
+    </main>
   )
 }
