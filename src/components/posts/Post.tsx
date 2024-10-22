ba880bc4914689643538906c8759b5040c5dc52f
Andranik Virabyan <gmparstone99@gmail.com>
2024-10-22 09:52:43 -0800

    Post image / video  adding future added

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -1,55 +1,114 @@
-"use client"
+"use client";
 
 import { PostData } from "@/lib/types";
 import Link from "next/link";
 import UserAvatar from "../UserAvatar";
-import { formatRelativeDate } from "@/lib/utils";
+import { cn, formatRelativeDate } from "@/lib/utils";
 import { useSession } from "@/app/(main)/SessionProvider";
 import PostMoreButton from "./PostMoreButton";
 import Linkify from "../Linkify";
 import UserTooltip from "../UserTooltip";
+import { Media } from "@prisma/client";
+import Image from "next/image";
 
 interface PostProps {
-    post: PostData;
+  post: PostData;
 }
 
-export default function Post({post}: PostProps) {
+export default function Post({ post }: PostProps) {
+  const { user } = useSession();
 
-    const {user} = useSession();
-
-    return <article className="group/post space-y-3 rounded-2xl bg-card p5
-    shadow-sm">
-        <div className="flex justify-between gap-3">
+  return (
+    <article className="group/post p5 space-y-3 rounded-2xl bg-card shadow-sm">
+      <div className="flex justify-between gap-3">
         <div className="flex flex-wrap gap-3">
-            <UserTooltip user={post.user}>
+          <UserTooltip user={post.user}>
             <Link href={`/users/${post.user.username}`}>
-                <UserAvatar avatarUrl={post.user.avatarUrl}/>
+              <UserAvatar avatarUrl={post.user.avatarUrl} />
             </Link>
-            </UserTooltip>
-            <div >
+          </UserTooltip>
+          <div>
             <UserTooltip user={post.user}>
-                <Link href={`/users/${post.user.username}`}
-                className="block font-medium hover:underline">
-                    {post.user.displayName}
-                </Link>
-                </UserTooltip>
-                <Link 
-                href={`/posts/${post.id}`}
-                className="block text-sm text-muted-foreground hover:underline ">
-                    {formatRelativeDate(post.createdAt)}
-                </Link>
-            </div>
+              <Link
+                href={`/users/${post.user.username}`}
+                className="block font-medium hover:underline"
+              >
+                {post.user.displayName}
+              </Link>
+            </UserTooltip>
+            <Link
+              href={`/posts/${post.id}`}
+              className="block text-sm text-muted-foreground hover:underline"
+            >
+              {formatRelativeDate(post.createdAt)}
+            </Link>
+          </div>
         </div>
         {post.user.id === user.id && (
-            <PostMoreButton
+          <PostMoreButton
             post={post}
-            className="opacity-0 transition-opacity group-hover/post:opacity-100" />
+            className="opacity-0 transition-opacity group-hover/post:opacity-100"
+          />
         )}
-        </div>
-        <Linkify>
+      </div>
+      <Linkify>
         <div className="whitespace-pre-line break-words px-5 pb-3">
-            {post.content}
+          {post.content}
         </div>
-        </Linkify>
+      </Linkify>
+      {!!post.attachments.length && (
+        <MediaPreviews attachments={post.attachments} />
+      )}
     </article>
+  );
+}
+
+interface MediaPreviewsProps {
+  attachments: Media[];
+}
+
+function MediaPreviews({ attachments }: MediaPreviewsProps) {
+  return (
+    <div
+      className={cn(
+        "flex flex-col gap-3",
+        attachments.length > 1 && "sm:grid sm:grid-cols-2",
+      )}
+    >
+      {attachments.map((m) => (
+        <MediaPreview key={m.id} media={m} />
+      ))}
+    </div>
+  );
+}
+
+interface MediaPreviewProps {
+  media: Media;
+}
+
+function MediaPreview({ media }: MediaPreviewProps) {
+  if (media.type === "IMAGE") {
+    return (
+      <Image
+        src={media.url}
+        alt="attachment"
+        width={500}
+        height={500}
+        className="mx-auto size-fit max-h-[30rem] rounded-2xl"
+      />
+    );
+  }
+  if (media.type === "VIDEO") {
+    return (
+      <div>
+        <video
+          src={media.url}
+          controls
+          className="mx-auto size-fit max-h-[30rem] rounded-2xl"
+        />
+      </div>
+    );
+  }
+
+  return <p className="text-destructive">Unsupported media type</p>;
 }
