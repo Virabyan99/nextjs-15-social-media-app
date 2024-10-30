7e2b46b29b3ae390aa5a7bdcc47e16fd971427c2
Andranik Virabyan <gmparstone99@gmail.com>
2024-10-30 07:21:35 -0800

    Google Auth added

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,21 +1,21 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-import {formatDate, formatDistanceToNowStrict} from "date-fns"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { formatDate, formatDistanceToNowStrict } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export function formatRelativeDate(from: Date) {
   const currentDate = new Date();
 
-  if(currentDate.getTime() - from.getTime() < 24 * 60 * 60 * 1000) {
-    return formatDistanceToNowStrict(from, {addSuffix: true})
+  if (currentDate.getTime() - from.getTime() < 24 * 60 * 60 * 1000) {
+    return formatDistanceToNowStrict(from, { addSuffix: true });
   } else {
-    if(currentDate.getFullYear() === from.getFullYear()) {
-      return formatDate(from, "MMM d")
+    if (currentDate.getFullYear() === from.getFullYear()) {
+      return formatDate(from, "MMM d");
     } else {
-      return formatDate(from, "MMM d, yyyy")
+      return formatDate(from, "MMM d, yyyy");
     }
   }
 }
@@ -24,5 +24,12 @@ export function formatNumber(n: number): string {
   return Intl.NumberFormat("en-US", {
     notation: "compact",
     maximumFractionDigits: 1,
-  }).format(n)
+  }).format(n);
+}
+
+export function slugify(input: string): string {
+  return input
+    .toLowerCase()
+    .replace(/ /g, "-")
+    .replace(/[^a-z0-9-]/g, "");
 }
